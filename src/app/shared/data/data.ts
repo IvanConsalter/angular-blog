85661c10f3173866881caccc848321d9a3c7ce2d
Ivan Consalter <ivanconsalter1@gmail.com>
2023-09-10 15:45:38 -0300

    added home component

---
+++ src/app/shared/data/data.ts
@@ -0,0 +1,26 @@
+export const data = [
+  {
+    "id":"1",
+    "titulo": "Tesla Cybertruck: vídeo mostra interior do veículo destruído",
+    "descricao": "Picape elétrica da Tesla teria passado por teste de capotamento, sugere vazamento",
+    "texto": "O Tesla Cybertruck está cada vez mais perto de chegar ao mercado. Na última semana, um vídeo vazado nas redes sociais sugere que o veículo passou por um teste de capotamento em vala.",
+    "fonte": "Olhar Digital",
+    "imgUrl":"https://img.olhardigital.com.br/wp-content/uploads/2023/05/Cybertruck.png"
+  },
+  {
+    "id":"2",
+    "titulo": "Como a Nasa está se preparando para trazer um pedaço de asteroide para o deserto",
+    "descricao": "Asteroides como o que está sendo explorado teriam colidido com a Terra no início da sua formação, e amostras colhidas podem revelar informações sobre o início do sistema solar",
+    "texto": "Uma amostra de asteroide armazenada dentro de uma espaçonave da Nasa está prestes a chegar à Terra depois de viajar por quase dois anos e meio pelo espaço.",
+    "fonte": "CNN Brasil",
+    "imgUrl":"https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2023/09/230906122917-03-nasa-osiris-rex-sample-training-model.webp?w=876&h=484&crop=1"
+  },
+  {
+    "id":"3",
+    "titulo": "Machine learning pode ser a chave para encontrar sinais de vida fora da Terra",
+    "descricao": "O machine learning, ou aprendizado de máquina, pode ser a chave para a descoberta de formas de vida diferentes daquelas que conhecemos até agora.",
+    "texto": "De acordo com um estudo publicado aceito na revista científica Astrophysical Journal e publicado no servidor de pré-impressão arXiv, uma equipe de cientistas sugeriu que os próximos estudos astronômicos podem utilizar machine learning para buscar por diferentes anomalias a partir de dados espectroscópicos de alta resolução.",
+    "fonte": "Tech tudo",
+    "imgUrl":"https://tm.ibxk.com.br/2023/09/06/06091403518075.jpeg?ims=1120x420"
+  }
+]
